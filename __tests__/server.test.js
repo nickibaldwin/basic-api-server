8bd7404e61120539311b8af6fe682e557f69440e
Nicki Baldwin <nickibaldwin@gmail.com>
2021-04-21 21:18:53 -0700

    needs tests

---
+++ __tests__/server.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//server-test.js
+
+//=========201=======//
+// it('should create a new item in the db', async() => {
+//   const response = await mockRequest.post('/animal').send({type: 'animal_test'});
+//   expect(response.status).tobe(201);
+//   expect(response.body.record.name).toEqual('animal_test')
+
+//   console.log(response.body);
+// })
--- src/server.js
+++ src/server.js
@@ -2,23 +2,28 @@
 
 //server.js
 const express = require('express');
+const app = express();
 
-//TODO check this file path
-const Thing = require('../models/things.js');
-const things = new Thing();
+const logger = require('./middleware/logger.js');
+const animalRoute = require('./routes/animal-route.js');
+const personRoute = require('./routes/person-route.js');
+
+const notFound = require('./errors/404.js');
+const errors = require('./errors/500.js');
 
 const router = express.Router();
 
-//TODO add route for person
+
 app.use(logger);
-// app.use(customeRoutes);
+app.use(animalRoute);
+app.use(personRoute);
 
 app.use('*', notFound);
 app.use(errors);
 
 module.exports = {
-    server: app,
-    start: port => {
-        app.listen(port, () => console.log(`server up: ${port}`));
-    }
+  server: app,
+  start: port => {
+    app.listen(port, () => console.log(`server up: ${port}`));
+  }
 }
